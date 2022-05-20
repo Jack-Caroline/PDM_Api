0b206cc63212effce793f8882074e14dd148c7b7
jack <786518239@qq.com>
2022-05-20 17:08:47 +0800

    编写新增项目测试用例

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -4,6 +4,7 @@
 # Create Time: 2022/5/15 上午 02:33
 import pytest
 import requests
+import random
 from common.handle_config import conf
 from common.handle_log import log
 
@@ -23,8 +24,19 @@ def login_setup():
     response = session.post(url=url, json=params, headers=headers)
     log.error(response.text)
     yield session
-    session.close()
 
 
-# if __name__ == '__main__':
-#     login()
+@pytest.fixture(scope='class')
+def project_side(login_setup):
+    session_side = login_setup
+    name = "上海科学技术有限责任公司{}".format(random.randint(0,100))
+    url = r"https://chalseenew_pdm.icwn.cn/Api/services/app/ProjectSide/SaveProjectSide"
+    params = {"Ownerships": [
+        {"HolderName": "周海明", "subscriptionAmount": 1000, "amountPaidIn": 1000, "ContriWay": "现金", "ContriRatio": 100,
+         "Member": ""}], "IsHighTech": 0, "CompanyName": name, "legalPerson": "周海明", "FoundDate": "2020-4-29",
+              "RegiCapital": 10000, "isHighTech": 2, "Communicator": "赵其他", "ComunicateJob": "法务",
+              "ComunicateTel": "13928883942", "Category": "制造业"}
+    response = session_side.post(url=url, json=params)
+    res = response.json()
+    yield session_side, res["result"]["id"]
+    session_side.close()
